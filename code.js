89a00c578957c567c404bd1c0c990bad6d195286
Hamaffin <101374544+Hamaffin@users.noreply.github.com>
2024-06-01 23:43:20 -0400

    code.js

--- code.js
+++ code.js
@@ -1,13 +1,22 @@
 var msgPlayer = document.getElementById("msgDisplay");
 var msgComp = document.getElementById("compDisplay");
 var resultPrint = document.getElementById("result");
+var scorePrint = document.getElementById("scoreDisplay");
 var compMove = ""
 var compResult = ""
+var result = ""
+var username = "Rishaan!"
 
 function playerPick(playerMove){
 playerChoice="You clicked "+playerMove+"!";
 msgPlayer.innerText = playerChoice
 var compResult = compPick()
+
+let score = JSON.parse(localStorage.getItem('score')) || {wins: 0,
+                                                         losses: 0,
+                                                         ties: 0}
+const welcomemsg = document.getElementById("welcome");
+welcomemsg.innerText = "Welcome "+(username === ""? "guest": username);  
 
 if (playerMove === "Rock"){
     if (compMove === "Rock"){
@@ -52,8 +61,24 @@ if (playerMove === "Scissors"){
     result = "You win!"
     resultPrint.innerText = result
  }
-;}
-;}
+}
+
+if (result === "You win!"){
+   score.wins +=1;
+   document.title = "Hooray!"
+}
+else if (result === "You lose!"){
+   score.losses +=1;
+   document.title = "Uh oh!"
+}
+else if (result === "It's a tie!"){
+   score.ties +=1;
+   document.title = "Tiebreaker!"
+}
+
+localStorage.setItem('score', JSON.stringify(score));
+scorePrint.innerText = 'Wins: '+score.wins+' Losses: '+score.losses+' Ties: '+score.ties
+}
 
 function compPick(){
 const compChoice = Math.random()
